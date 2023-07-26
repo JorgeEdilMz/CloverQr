6142bb4e358f928973c31fdcf53567267ef1da48
JESSICA2436 <jimenezjessicatatiana680@gmail.com>
2023-07-26 16:55:55 -0500

    agregar actividades

--- app/src/main/java/com/clover/cloverqr/PlantList.java
+++ app/src/main/java/com/clover/cloverqr/PlantList.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link adminFragment#newInstance} factory method to
+ * Use the {@link PlantList#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class adminFragment extends Fragment {
+public class PlantList extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class adminFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public adminFragment() {
+    public PlantList() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class adminFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment adminFragment.
+     * @return A new instance of fragment PlantList.
      */
     // TODO: Rename and change types and number of parameters
-    public static adminFragment newInstance(String param1, String param2) {
-        adminFragment fragment = new adminFragment();
+    public static PlantList newInstance(String param1, String param2) {
+        PlantList fragment = new PlantList();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class adminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false);
+        return inflater.inflate(R.layout.fragment_plant_list, container, false);
     }
 }
